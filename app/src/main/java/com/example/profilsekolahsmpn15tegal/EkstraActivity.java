961db2aa546f24ee505fb4c3956d4d523d489029
Davidade08 <davidadeprasetio@gmail.com>
2021-02-04 11:05:18 +0700

    koneksi  halaman 2

---
+++ app/src/main/java/com/example/profilsekolahsmpn15tegal/EkstraActivity.java
@@ -0,0 +1,14 @@
+package com.example.profilsekolahsmpn15tegal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EkstraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ekstra);
+    }
+}
--- app/src/main/java/com/example/profilsekolahsmpn15tegal/MainActivity.java
+++ app/src/main/java/com/example/profilsekolahsmpn15tegal/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.profilsekolahsmpn15tegal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-}
+    public void profil(View view){
+        Intent intent=new Intent(MainActivity.this, ProfilActivity.class);
+        startActivity(intent);
+    }
+    public void sejarah(View view){
+        Intent intent=new Intent(MainActivity.this, SejarahActivity.class);
+        startActivity(intent);
+    }
+    public void ekstra(View view){
+        Intent intent=new Intent(MainActivity.this, EkstraActivity.class);
+        startActivity(intent);
+    }
+    public void prestasi(View view){
+        Intent intent=new Intent(MainActivity.this, PrestasiActivity.class);
+        startActivity(intent);
+    }
+    public void absensi(View view){
+        Intent intent=new Intent(MainActivity.this, AbsensiActivity.class);
+        startActivity(intent);
+    }
+    public void tugas(View view) {
+        Intent intent = new Intent(MainActivity.this, TugasActivity.class);
+        startActivity(intent);
+
+    }
+    }
